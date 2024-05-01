1200e457e3e767da770e9bddc06b6f182622a9ff
Sachitha Maheshkumar <155910259+SachCodeCraft@users.noreply.github.com>
2024-05-01 12:19:14 +0530

    Add files via upload

---
+++ exp3/Addition.java
@@ -0,0 +1,8 @@
+public class Addition {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 8;
+        int sum = a + b;
+        System.out.println("Sum of " + a + " and " + b + " is: " + sum);
+    }
+}
